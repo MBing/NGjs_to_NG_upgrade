e26c18f5ff3be1924be9058b65258050e0a44f04
Martin Bing <bing.martin.photography@gmail.com>
2018-01-27 18:15:53 +0100

    Fix 01 to run and update to the new @ui-router

--- 01-components/src/app/chat/chat.module.js
+++ 01-components/src/app/chat/chat.module.js
@@ -1,5 +1,5 @@
 import angular from 'angular';
-import uiRouter from 'angular-ui-router';
+import uiRouter from '@uirouter/angularjs';
 import MatchWindowHeight from './shared/utilities/match-window-height.directive';
 
 import ChatPageComponent from './containers/chat-page/chat-page.component';
